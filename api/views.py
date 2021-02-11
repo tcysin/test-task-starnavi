aa7a418012361373559ebb20b9051b3e770290b4
Aleksei Tcysin <aleksei.tcysin@gmail.com>
2021-02-11 17:55:17 +0300

    Add views and url for working with posts via api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+@csrf_exempt
+def post_list(request):
+    """List all posts, or create a new post."""
+
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+    
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def post_detail(request, pk):
+    """
+    Retrieve, update or delete a code post.
+    """
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
