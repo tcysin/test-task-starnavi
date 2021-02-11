67147326e405fe0a5ce54b7e583a6094b296e886
Aleksei Tcysin <aleksei.tcysin@gmail.com>
2021-02-11 17:40:04 +0300

    Create Post model and serializer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    # we delete all author posts when he vanishes
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200, blank=False)
+    body = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
